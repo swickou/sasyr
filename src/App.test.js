67b7cec3c9a6ef95d0f5499251d2e3ec286119ff
cedric <cedric.greufeille@gmail.com>
2021-09-14 21:06:30 +0200

    Config

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,11 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('l\'addition de nombres positifs n\'est pas égale à zéro', () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0);
+    }
+  }
+});
